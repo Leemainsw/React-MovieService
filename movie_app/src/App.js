851b3d4aa78f5aa732c35b66a11fd6420feb3d0c
Yeseul Lee <s2018s15@e-mirim.hs.kr>
2020-08-21 10:50:13 +0900

    #2.1 Reusable Components with JSX + Props
    JSX 알아보기

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({fav}){
+  console.log(fav);
+  return <h1>{ fav }</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
