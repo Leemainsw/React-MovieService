0c3a9cce57082d9df7b6c06c1548beb7ffe71e1b
Yeseul Lee <s2018s15@e-mirim.hs.kr>
2020-08-24 11:27:23 +0900

    #3.3 Planning the Movie Component
    componentDidMount()
    setTimeout()

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,61 +4,23 @@ import { number } from 'prop-types';
 // class component
 class App extends React.Component{
 
-  constructor(props){
-    super(props);
-    console.log('constructor');
-  }
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
   componentDidMount(){
-    console.log('component render')
+    setTimeout(()=>{
+      this.setState({ isLoading: false, book: true });
+
+    }, 6000)
   }
-
-  componentDidUpdate(){
-    console.log('component did update');
-  }
-
-  componentWillUnmount(){
-    console.log('componentWillUnmount')
-  }
-  
-  state = {
-    count : 0
-  };
-
-  add = () =>{
-    this.setState(current => ({ count : current.count + 1}));
-  };
-
-  minus = () => {
-    this.setState({count : this.state.count - 1});
-  };
-
 
   render(){
-    console.log('Im rendering');
-    return(
-      <div>
-        <h1>The Number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading ... " : "We are ready"}</div>
   }
-}
 
-// Function component
-// function App() {
-//   return (
-//     <div>
-//       <h1>Hello!</h1>
-//       {foodILike.map(result => <Food 
-//       key = {result.id}
-//       name = {result.name} 
-//       image = {result.image}
-//       rating = {result.rating}
-//       />)}
-//     </div>
-//   );
-// }
+}
 
 export default App;
